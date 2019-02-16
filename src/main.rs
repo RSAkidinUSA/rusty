5d660256ed4b2c40484e99bc04ba60b114658cdb
Ryan Burrow <rsardb11@vt.edu>
2019-02-16 17:07:53 -0500

    Made some changes after running cargo clippy

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ macro_rules! HELP_DOC {
 }
 
 macro_rules! VERSION_INFO {
-    () => ("{} (rusty tool) 0.1.0\n\
+    () => ("{} (rusty tool) 0.1.1\n\
     A basic implementation of yes written in rust.\n\
     License MIT\n\
     This is free software that was written to learn the language\n\
@@ -57,25 +57,26 @@ fn main() {
 
 fn arg_compare(name: &str, yes: &str) -> (bool, i32) {
     if yes.len() > 2 {        
-        if yes.as_bytes()[0] != '-' as u8 {
-            return (true, 0);
+        if yes.as_bytes()[0] != b"-"[0] {
+            (true, 0)
         }
-
-        match &yes[..=2] {
-            "--h" => {
-                print!(HELP_DOC!(), name, name);
-                return (false, 0)
-            },
-            "--v" => {
-                print!(VERSION_INFO!(), name);
-                return (false, 0)
-            },
-            _ => {
-                print!(INVALID_USAGE!(), name, yes.as_bytes()[1] as char, name);
-                return (false, 1)
-            },
-        };
+        else {
+            match &yes[..=2] {
+                "--h" => {
+                    print!(HELP_DOC!(), name, name);
+                    (false, 0)
+                },
+                "--v" => {
+                    print!(VERSION_INFO!(), name);
+                    (false, 0)
+                },
+                _ => {
+                    print!(INVALID_USAGE!(), name, yes.as_bytes()[1] as char, name);
+                    (false, 1)
+                },
+            }
+        }
     } else {
-        return (true, 0);
+        (true, 0)
     }
 }
